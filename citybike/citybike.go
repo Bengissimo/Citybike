2912c7d2a4cf291eafbf7f8a9d6cd2aafaeed8a1
Bengisu Kandemir <bkandemi@student.hive.fi>
2023-01-13 12:43:50 +0200

    CreateTables method added into citybike package

--- citybike/citybike.go
+++ citybike/citybike.go
@@ -1,4 +1,4 @@
-//Package citybike creates sqlite database
+// Package citybike creates sqlite database
 // and includes database related methods and functions
 package citybike
 
@@ -8,10 +8,29 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var (
+	createJourneyTable string = `DROP TABLE IF EXISTS JourneyList;
+	CREATE TABLE IF NOT EXISTS JourneyList (
+	id INTEGER PRIMARY KEY,
+	Departure_ID INTEGER,
+	Return_ID INTEGER,
+	Distance FLOAT,
+	Duration INTEGER)`
+	createStationTable string = `DROP TABLE IF EXISTS StationList;
+	CREATE TABLE IF NOT EXISTS StationList (
+	id INTEGER,
+	FI_Name TEXT,
+	SE_Name TEXT,
+	FI_Address TEXT,
+	SE_Address TEXT)`
+)
+
+// Citybike is a representation of sql.DB
 type Citybike struct {
 	db *sql.DB
 }
 
+// New opens a database using sql.Open assigns this database to the returned Citybike struct
 func New(path string) (*Citybike, error) {
 	sqldb, err := sql.Open("sqlite3", path)
 	if err != nil {
@@ -24,3 +43,20 @@ func New(path string) (*Citybike, error) {
 	return db, nil
 }
 
+// exec executes the create table query given as string argument
+func (citybike *Citybike) exec(createTable string) error {
+	_, err := citybike.db.Exec(createTable)
+	return err
+}
+
+// CreateTables creates Journey and Station tables in the Citybike database
+func (citybike *Citybike) CreateTables() error {
+	if err := citybike.exec(createJourneyTable); err != nil {
+		return err
+	}
+
+	if err := citybike.exec(createStationTable); err != nil {
+		return err
+	}
+	return nil
+}
