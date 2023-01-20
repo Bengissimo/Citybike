98394ea306372c9343d0624afd11c7949cd24ef0
Bengisu Kandemir <bkandemi@student.hive.fi>
2023-01-20 19:40:36 +0200

    /'Station not found/' error added to GetSingleStation function.

--- citybike/queries.go
+++ citybike/queries.go
@@ -1,6 +1,9 @@
 package citybike
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+)
 
 var (
 	PerPage        int    = 20
@@ -22,17 +25,6 @@ var (
 							INNER JOIN StationList AS Return ON JourneyList.Return_ID = Return.id
 							LIMIT ? OFFSET ?`
 	selectSingle  string = "SELECT FI_Name, SE_Name, FI_Address, SE_Address FROM StationList WHERE id = ?"
-	countStations string = "SELECT count(*) FROM StationList"
-	countJourneys string = `SELECT count(*)
-							FROM JourneyList
-							INNER JOIN StationList AS Departure ON JourneyList.Departure_ID = Departure.id
-							INNER JOIN StationList AS Return ON JourneyList.Return_ID = Return.id`
-	countJourneysStartFrom string = `SELECT count(*) 
-									FROM JourneyList
-									WHERE Departure_ID = ?`
-	countJourneysEndingAt string = `SELECT count(*) 
-									FROM JourneyList
-									WHERE Return_ID = ?`
 )
 
 func (citybike *Citybike) makeQuery(page int, perpage int, query string) (*sql.Rows, error) {
@@ -106,9 +98,13 @@ func (citybike *Citybike) GetSingleStation(id int) (*Station, error) {
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		return nil, errors.New("Station not found")
+	}
 
 	theStation := &Station{}
-	rows.Next()
 	rows.Scan(
 		&theStation.NameFI,
 		&theStation.NameSE,
@@ -116,70 +112,4 @@ func (citybike *Citybike) GetSingleStation(id int) (*Station, error) {
 		&theStation.AddressSE)
 
 	return theStation, nil
-}
-
-func (citybike *Citybike) CountStations() (int, error) {
-	rows, err := citybike.db.Query(countStations)
-	if err != nil {
-		return 0, err
-	}
-	defer rows.Close()
-
-	count := 0
-	rows.Next()
-	rows.Scan(&count)
-	return count, nil
-}
-
-func (citybike *Citybike) CountJourneys() (int, error) {
-	rows, err := citybike.db.Query(countJourneys)
-	if err != nil {
-		return 0, err
-	}
-	defer rows.Close()
-
-	count := 0
-	rows.Next()
-	rows.Scan(&count)
-	return count, nil
-}
-
-func (citybike *Citybike) CountStationJourney(id int) (int, int, error) {
-	startFrom, endingAt := 0, 0
-
-	stmt, err := citybike.db.Prepare(countJourneysStartFrom)
-	if err != nil {
-		return startFrom, endingAt, err
-	}
-	defer stmt.Close()
-
-	rows, err := stmt.Query(id)
-	if err != nil {
-		return startFrom, endingAt, err
-	}
-	defer rows.Close()
-
-	if !rows.Next() {
-		return startFrom, endingAt, nil
-	}
-	rows.Scan(&startFrom)
-
-	stmt, err = citybike.db.Prepare(countJourneysEndingAt)
-	if err != nil {
-		return startFrom, endingAt, err
-	}
-	defer stmt.Close()
-
-	rows, err = stmt.Query(id)
-	if err != nil {
-		return startFrom, endingAt, err
-	}
-	defer rows.Close()
-
-	if !rows.Next() {
-		return startFrom, endingAt, nil
-	}
-	rows.Scan(&endingAt)
-
-	return startFrom, endingAt, nil
 }
